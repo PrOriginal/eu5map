a13b3f2ed9d82cf210538a79e401bcb5df22edc3
LP <brofistua@gmail.com>
2026-01-06 15:07:30 +0100

    "First working version of a parser.py"

--- src/main/resources/tempDB/parser.py
+++ src/main/resources/tempDB/parser.py
@@ -1,7 +1,8 @@
+import json
 import os
 import re as regex
 class Location:
-    def __init__(self,name,topography,vegetation,climate,religion,culture,raw_material,natural_harbor_suitability = 0):
+    def __init__(self,name,topography,vegetation,climate,religion,raw_material,culture=None,natural_harbor_suitability = 0, modifier = None):
         self.name = name
         self.topography = topography
         self.vegetation = vegetation
@@ -9,25 +10,28 @@ class Location:
         self.religion = religion
         self.culture = culture
         self.raw_material = raw_material
-        self.natural_harbor_suitability = natural_harbor_suitability
+        self.natural_harbor_suitability = float(natural_harbor_suitability)
+        self.modifier = modifier
 
-regex_valid_location = "^.+material.+$"
+    def to_dict(self):
+        return self.__dict__
+
+regex_valid_location = "^.+material.+$" #r"(\w+)\s*=\s*\{([^}]+)\}"
 regex_location_name = "^\\w+"
-regex_location_feature = ""
-location_info = []
+regex_location_params = "(\w+)\s*=\s*([\w\d.]+)"
+
+with open("location_templates.txt") as f:
+    valid_locations = [line for line in f if regex.findall(regex_valid_location, line)]
+
+locations_objects = []
+
+for location in valid_locations:
+    name = regex.findall(regex_location_name, location)
+    params = dict(regex.findall(regex_location_params,location))
+    locations_objects.append(Location(name=name, **params))
 
 if os.path.exists("locations.json"):
     os.remove("locations.json")
-db = open("locations.json", "x")
 
-with open("location_templates.txt") as source:
-    valid_locations = [line for line in source if regex.findall(regex_valid_location, line)]
-    print(valid_locations[1])
-
-        # with open("locations.json", "w") as new_db:
-        # print(locations)
-        # for line in source:
-        #     name = regex.findall(regex_location_name, line)
-        #     print(name)
-        #     new_db.write("\n---------------------------------------------\n")
-        #     new_db.write(name[0])
+with open("locations.json", "w") as f:
+        f.write(json.dumps([location.to_dict() for location in locations_objects]))
